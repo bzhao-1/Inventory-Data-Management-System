6ddd60ea5548ea177e8ecb6b194f17d613e7e451
Ben Zhao <96789119+bzhao-1@users.noreply.github.com>
2022-08-05 15:37:36 -0400

    Add files via upload

--- Python Script/inventoryautomate.py
+++ Python Script/inventoryautomate.py
@@ -3,7 +3,7 @@
 ## Python Script to Automate Inventory Tracking for Biological Instruments at 700 HQ 
 ## Update Inventory2022 sheet with date and number of items stocked in shelf in lab room, then run script and will return updated spreadsheet with remaining number of items in main storage room 
 ## When new shipments arrive, add shipment amount to total in "Last" column for row with the item in Inventory2022 sheet then run script will get updated remaining amounts
-## Make sure to delete previous Inventory2022Updated in folder before running script so that most recent updates can be added to the new generated spreadsheet 
+## Make sure to close updated spreadsheet beforing running script or updates can not be generated 
 ## Contact @benzhao90@gmail.com or 3474717778 with any questions 
 
 import pandas as pd
@@ -20,7 +20,19 @@ dict_df = pd.read_excel('Inventory2022.xlsx', sheet_name=['Gloves.','Cleaning','
 def remaining(a, b):
     return b - a
 
+def highlight(val):
+    if val <= 20:
+        color = 'blue'
+    elif val <= 10:
+        color = 'yellow'
+    elif val <= 5:
+        color = 'red'
+    else:
+        color = 'transparent'
+    return 'background-color: {}'.format(color)
+
 def main(dict_df):
+    writer = pd.ExcelWriter('Inventory2022Updated.xlsx', engine='xlsxwriter', date_format='dd mmm yyyy')
     for i in dict_df:
         df_gloves = dict_df.get(i)
         df_gloves['Last'] = df_gloves['Last'].fillna(0)
@@ -32,12 +44,9 @@ def main(dict_df):
             df_gloves['Remaining in Stock'] = df_gloves.apply(
                 lambda x: remaining(x['Boxes put on shelf'], x['Last']), axis=1)
         df_gloves = df_gloves.drop(['Last'], axis=1, inplace=True)
-    
-    writer = pd.ExcelWriter('Inventory2022Updated.xlsx', engine='xlsxwriter', date_format='dd mmm yyyy')
-    for i in dict_df:
-        name = i
         df = dict_df.get(i)
-        df.to_excel(writer, sheet_name = name)
+        df.style.applymap(highlight, subset=['Remaining in Stock']).to_excel(writer, sheet_name = i)
+    
     workbook = writer.book
     fmt_number = workbook.add_format({"num_format": "0"})
     fmt_header = workbook.add_format({'bold': True,
@@ -47,9 +56,6 @@ def main(dict_df):
     'border': 1})
     for i in dict_df:
         worksheet = writer.sheets[i]
-        df = dict_df.get(i)
-        # for i, v in enumerate(df.columns.values):
-        #     worksheet.write(0, i, v, fmt_header)
         worksheet.set_column("B:L", 20)
         worksheet.set_column("G:G",20, fmt_number)
     writer.save()
